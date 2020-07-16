6d86fccf098eab9377a318ff39c1bec707a4783a
perfectcharm <57660209+perfectcharm@users.noreply.github.com>
2020-07-16 20:20:54 +0300

    Start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './table.js';
 
+const variable = [
+  { name: "доход" },
+  { name: "расход"},
+  { name: "за последний месяц"},
+  { name: "транзакция более 1000 руб."}, 
+];
+
+function TransactionDisplay() {
+  return (
+    <h1> Display Transaction <script src="/table.js"></script> </h1>
+  )
+}
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {variable.map((name, index) => 
+        <button
+        key={index}
+        onClick={() => {
+          console.log("Clicked name " + index);
+        }}
         >
-          Learn React
-        </a>
-      </header>
+          {place.name}
+        </button>
+      )
+
+      }
     </div>
   );
 }
