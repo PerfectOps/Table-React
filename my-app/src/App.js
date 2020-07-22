8d0e834c4df58ba6602ac3d376ef41967afd83b8
perfectcharm <57660209+perfectcharm@users.noreply.github.com>
2020-07-22 16:42:59 +0300

    Start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import arr from './table.js';
-import {BootstrapTable, TableHeaderColumn} 
-       from 'react-bootstrap-table';
+import { arr } from './table';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: [arr]}
+  }
   handleBtnClick1 = () => {
-    this.refs.table.handleFilterData({ type: 'доход' })
+    this.refs.table.handleFilterData({ type: "доход" })
   }
   handleBtnClick2 = () => {
-    this.refs.table.handleFilterData({ type: 'расход' })
+    this.refs.table.handleFilterData({ type: "расход" })
   }
   handleBtnClick3 = () => {
-    this.refs.table.handleFilterData({ date: '07.2020' })
+    this.refs.table.handleFilterData({ date: "07.2020" })
   }
-  handleBtnClick4 = () => {
-    const price = arr.filter(item => item.value > 1000)
-    this.refs.table.handleFilterData({ value: price })
-  }
+  // handleBtnClick4 = () => {
+  //   this.refs.table.handleFilterData({ value: >1000 })
+  // }
   render() {
     return (
       <div>
@@ -28,14 +31,14 @@ class App extends React.Component {
           <TableHeaderColumn dataField='type'>Type</TableHeaderColumn>
           <TableHeaderColumn dataField='date'>Date</TableHeaderColumn>
         </BootstrapTable>
-        <div class="btn-group" role="group" aria-label="Basic example">
-        <button onClick={ this.handleBtnClick1 } className='btn btn-default'>доход</button>
-        <button onClick={ this.handleBtnClick2 } className='btn btn-default'>расход</button>
-        <button onClick={ this.handleBtnClick3 } className='btn btn-default'>за последний месяц</button>
-        <button onClick={ this.handleBtnClick4 } className='btn btn-default'>более 1000руб.</button>
+        <div>
+        <button onClick={ this.handleBtnClick1 } className='btn btn-default'>{"доход"}</button>
+        <button onClick={ this.handleBtnClick2 } className='btn btn-default'>{"расход"}</button>
+        <button onClick={ this.handleBtnClick3 } className='btn btn-default'>{"за последний месяц"}</button>
+        {/* <button onClick={ this.handleBtnClick4 } className='btn btn-default'>{"более 1000руб."}</button> */}
         </div>
       </div>
-    );
+    )
   }
 }
 
--- my-app/src/table.js
+++ my-app/src/table.js
@@ -1,44 +1,64 @@
 const arr = [
   { 
-    id: 1, value: 23000, type: "расход", 
+    id: 1, 
+    value: 23000, 
+    type: "расход", 
     date: "26.04.2020 23:15" 
   },
   { 
-    id: 2, value: 5400, type: "расход", 
-  date: "30.04.2020 08:01" 
+    id: 2, 
+    value: 5400, 
+    type: "расход", 
+    date: "30.04.2020 08:01" 
   },
   { 
-    id: 3, value: 32650, type: "доход", 
+    id: 3, 
+    value: 32650, 
+    type: "доход", 
     date: "01.05.2020 12:30" 
   },
   { 
-    id: 4, value: 522, type: "расход", 
+    id: 4, 
+    value: 522, 
+    type: "расход", 
     date: "01.05.2020 14:59" 
   },
   { 
-    id: 5, value: 50000, type: "доход", 
+    id: 5, 
+    value: 50000, 
+    type: "доход", 
     date: "10.05.2020 18:00" 
   },
   { 
-    id: 6, value: 20000, type: "доход", 
+    id: 6, 
+    value: 20000, 
+    type: "доход", 
     date: "13.06.2020 17:07" 
   },
   { 
-    id: 7, value: 10500, type: "расход", 
+    id: 7, 
+    value: 10500, 
+    type: "расход", 
     date: "14.06.2020 05:00" 
   },
   { 
-    id: 8, value: 750, type: "расход", 
+    id: 8, 
+    value: 750, 
+    type: "расход", 
     date: "18.06.2020 12:03" 
   },
   { 
-    id: 9, value: 15, type: "расход", 
+    id: 9, 
+    value: 15, 
+    type: "расход", 
     date: "10.07.2020 17:34" 
   },
   { 
-    id: 10, value: 40000, type: "доход", 
+    id: 10, 
+    value: 40000, 
+    type: "доход", 
     date: "11.07.2020 18:04" 
   }
 ]
 
-export default arr
+export { arr }
